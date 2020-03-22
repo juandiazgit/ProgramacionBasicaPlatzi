14eef33305158fc33d072415d7b91df482bcff45
Juan Diaz <juangdm1234@gmail.com>
2020-03-21 19:11:23 -0500

    Adicion archivos .html y .js

---
+++ villaplatzi/platzi.js
@@ -0,0 +1,12 @@
+var z;
+
+for (var i = 0; i < 10; i++) 
+{
+    z = aleatorio(5,0);
+    document.write(z+", ");
+}
+
+function aleatorio(min,maxi) {
+    var resultado = Math.floor( Math.random() * ( maxi - min + 1 ) ) + min;
+    return resultado;
+}
