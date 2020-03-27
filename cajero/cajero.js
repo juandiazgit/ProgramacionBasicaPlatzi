377fda8d2d92521c496ec546484c783bd88aa6dc
Juan Diaz <juangdm1234@gmail.com>
2020-03-26 23:17:33 -0500

    Adicion contabilizacion cajero

--- cajero/cajero.js
+++ cajero/cajero.js
@@ -16,6 +16,14 @@ class Billete
 
 function entregarDinero()
 {
+    if (totalCantidadBilletes(caja) == 0)
+    {
+        resultado.innerHTML = "";
+        mostrarDinero.innerHTML = "";
+        cajaVacia.innerHTML = "<h3>Cajero quedó sin dinero !!!</h3>";
+        return;
+    }
+
     var t = document.getElementById("dinero");
     dinero = parseInt(t.value);
 
@@ -37,6 +45,7 @@ function entregarDinero()
         }
     }
 
+    resultado.innerHTML = "";
     if (dinero > 0) 
     {
         resultado.innerHTML = "No contamos con dinero o billetes suficiente!!!";
@@ -54,22 +63,68 @@ function entregarDinero()
                 }
             }
         }
+        resultado.innerHTML += "<hr/>";
+        
+        caja = actualizarDineroCaja(caja,entregado);
+        entregado = [];
     }
+    mostrarDineroCaja(caja);
+}
+
+function mostrarDineroCaja(mcaja)
+{
+    mostrarDinero.innerHTML = "*****El cajero actualmente cuenta con: ***** <br/>";
+    for (var ob of mcaja)
+    {
+        if (ob.cantidad > 0) 
+        {
+            mostrarDinero.innerHTML += ob.cantidad+" Billetes de $ "+ob.valor+" = "+ob.cantidad*ob.valor+" <br/>";
+        }
+    }
+}
+
+function actualizarDineroCaja(acaja,aentregado)
+{
+    for (var ae of aentregado)
+    {
+        for (var ac of acaja) 
+        {
+            if ((ae.valor == ac.valor) && (ae.cantidad > 0))
+            {
+                ac.cantidad -= ae.cantidad;
+                break;
+            }
+        }
+    }
+    return acaja;
+}
+
+function totalCantidadBilletes(tcaja) 
+{
+    let total = 0;
+    for (var obj of tcaja) 
+    {
+        total += obj.cantidad;
+    }
+    return total;
 }
 
 var caja = [];
 var entregado = [];
 
-caja.push(new Billete(100,5));
-caja.push(new Billete(50,10));
-caja.push(new Billete(20,5));
-caja.push(new Billete(10,20));
-caja.push(new Billete(5,5));
+caja.push(new Billete(100,2));
+caja.push(new Billete(50,5));
+caja.push(new Billete(20,2));
+caja.push(new Billete(10,3));
+caja.push(new Billete(5,4));
 
 var dinero = 0;
 var div = 0;
 var papeles = 0;
 
 var resultado = document.getElementById("resultado");
+var mostrarDinero = document.getElementById("mostrarDinero");
+var cajaVacia = document.getElementById("cajaVacia");
 var b = document.getElementById("extraer");
+mostrarDineroCaja(caja);
 b.addEventListener("click",entregarDinero);
